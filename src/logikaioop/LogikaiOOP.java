c9db6d088b1c143a279e65482d250bfdb451df29
Veress Marcell <veres.marcell@dk.akkszalezi.hu>
2024-01-08 14:53:06 +0100

    uml ábra szerint

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -3,14 +3,14 @@ package logikaioop;
 public class LogikaiOOP {
 
     public static void main(String[] args) {
-        Allitas[] allitasok = new Allitas[3];
-        allitasok[0] = new Allitas("Az 1. állítás!");
-        allitasok[1] = new Allitas("A 2. állítás!");
-        allitasok[2] = new Allitas("A 3. állítás!");
+        Lada[] allitasok = new Lada[3];
+        allitasok[0] = new Lada("Az 1. állítás!");
+        allitasok[1] = new Lada("A 2. állítás!");
+        allitasok[2] = new Lada("A 3. állítás!");
 
         System.out.println("Leírás");
-        for (Allitas allitas : allitasok) {
-            System.out.println(allitas);
+        for (Lada allitas : allitasok) {
+            System.out.println(allitas.getAllitas());
         }
         System.out.println("Melyik:");
         System.out.println("A válasz jó | rossz");
