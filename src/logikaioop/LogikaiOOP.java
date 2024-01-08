c0112a9073afe5db2814439d306aa4a68af78992
Veress Marcell <veres.marcell@dk.akkszalezi.hu>
2024-01-08 13:46:40 +0100

    +scanner

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -1,13 +1,17 @@
 package logikaioop;
 
+import java.util.Scanner;
+
 public class LogikaiOOP {
 
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Leírás");
         System.out.println("Az 1. állítás!");
         System.out.println("A 2. állítás!");
         System.out.println("A 3. állítás!");
-        System.out.println("Melyik:");
+        System.out.print("Melyik: ");
+        sc.next();
         System.out.println("A válasz jó | rossz");
     }
     
