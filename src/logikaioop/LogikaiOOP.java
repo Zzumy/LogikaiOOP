3e7eb53274b834141568da6bf392e9884a2ef391
Nguyen Duc Duy <nguyen.d.duy@dk.akkszalezi.hu>
2024-01-08 15:22:35 +0100

    conflict megoldasa

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class LogikaiOOP {
 
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+        Lada[] allitasok = new Lada[3];
+        allitasok[0] = new Lada("Az 1. állítás!");
+        allitasok[1] = new Lada("A 2. állítás!");
+        allitasok[2] = new Lada("A 3. állítás!");
+
         System.out.println("Leírás");
-        System.out.println("Az 1. állítás!");
-        System.out.println("A 2. állítás!");
-        System.out.println("A 3. állítás!");
-        System.out.print("Melyik: ");
-        sc.next();
+        for (Lada allitas : allitasok) {
+            System.out.println(allitas.getAllitas());
+        }
+        System.out.println("Melyik:");
         System.out.println("A válasz jó | rossz");
     }
-    
+
 }
